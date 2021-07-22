17796f0be1fecddef9710b5420224df321c340e6
corrigan-r <rcorr073@uottawa.ca>
2021-07-22 16:54:24 -0400

    updated code

--- code.R
+++ code.R
@@ -5,16 +5,19 @@
 
 # ##########################################################
 # # Create edx set, validation set (final hold-out test set)
-# # (this section of code provided by HarvardX)
+# # (this first section of code provided by HarvardX)
 # ##########################################################
 
 if(!require(tidyverse)) install.packages("tidyverse", repos = "http://cran.us.r-project.org")
 if(!require(caret)) install.packages("caret", repos = "http://cran.us.r-project.org")
 if(!require(data.table)) install.packages("data.table", repos = "http://cran.us.r-project.org")
 
-library(tidyverse)
 library(caret)
 library(data.table)
+library(dplyr)
+library(ggplot2)
+library(lubridate)
+library(tidyverse)
 
 # MovieLens 10M dataset:
 # https://grouplens.org/datasets/movielens/10m/
@@ -60,17 +63,12 @@ sapply(patterns, function(x){
   edx$genres %>% str_detect(x)  %>% sum()
 })
 
-# #####################################################
-# # load additional libraries
-# #####################################################
-library(dplyr)
-library(ggplot2)
-library(lubridate)
 
 # #####################################################
-# # useful functions
+# # Useful functions
 # #####################################################
 # compute root mean squared error
+
 RMSE <- function(true_ratings, predicted_ratings){
   sqrt(mean((true_ratings - predicted_ratings)^2))
 }
@@ -79,10 +77,15 @@ RMSE <- function(true_ratings, predicted_ratings){
 # #####################################################
 # # create train and test sets from edx
 # #####################################################
+
 set.seed(2021)
+
 test_mndex <- createDataPartition(edx$userId, times = 1, p = 0.1, list = FALSE)
+
 test_set <- edx[test_mndex, ]
+
 train_set <- edx[-test_mndex, ]
+
 train_set <- train_set %>% mutate(quarter = round_date(as_datetime(timestamp), 
   "quarter")) # add column for quarter
 
@@ -90,6 +93,7 @@ train_set <- train_set %>% mutate(quarter = round_date(as_datetime(timestamp),
 # #####################################################
 # # effects calculations prior to regularization 
 # #####################################################
+
 initial_default_pentalty <- 3 # set an initial default penalty of 3
 lambda_m <- initial_default_pentalty # init. set regularization penalty parameter to 3 
 lambda_g <- initial_default_pentalty # init. set regularization penalty parameter to 3
@@ -97,7 +101,7 @@ lambda_u <- initial_default_pentalty # init. set regularization penalty paramete
 
 mu <- mean(train_set$rating)
 
-# compute initial user, genre, and time effects 
+# compute initial movie, user, genre, and time effects 
 # (based on initial lambdas, prior to regularization)
 
 # compute movie effects
@@ -123,9 +127,11 @@ quarter_avgs <- train_set %>% filter(quarter != as_date("1995-01-01")) %>%
   left_join(genre_avgs, by = 'genres')%>%
   group_by(quarter) %>% summarize(b_q = mean(rating - mu - b_m - b_u - b_g)) # no regularization for quarter
 
+
 # #####################################################
 # # regularization 
 # #####################################################
+
 lambdas <- seq(0,20,1) # perform calcs for range of reg. penalties
 
 # regularization for userId
@@ -143,6 +149,7 @@ rmses_u <- sapply(lambdas, function(l){
   predicted_ratings[is.na(predicted_ratings)] <- mu # assign ave rating to NA
    return(RMSE(predicted_ratings, test_set$rating))
 })
+
 lambda_u_optimal <- lambdas[which.min(rmses_u)] # select optimal penalty
 
 # regularization for movieId
@@ -161,6 +168,7 @@ rmses_m <- sapply(lambdas, function(l){
   predicted_ratings[is.na(predicted_ratings)] <- mu # assign ave rating to NA
   return(RMSE(predicted_ratings, test_set$rating)) 
   })
+
 lambda_m_optimal <- lambdas[which.min(rmses_m)] # select optimal penalty
 
 # regularization for genre effects
@@ -182,7 +190,6 @@ rmses_g <- sapply(lambdas, function(l){
 lambda_g_optimal <- lambdas[which.min(rmses_g)] # select optimal penalty
 
 
-
 # #####################################################
 # compute effects using optimal lambdas
 # #####################################################
@@ -201,7 +208,8 @@ user_avgs <- train_set %>%
 # compute genre effects
 genre_avgs <- train_set %>% left_join(movie_avgs,by = "movieId") %>%
   left_join(user_avgs, by = 'userId') %>% 
-  group_by(genres)%>% summarize(b_g = sum(rating - mu - b_m - b_u)/(n() + lambda_g_optimal))
+  group_by(genres) %>% 
+  summarize(b_g = sum(rating - mu - b_m - b_u)/(n() + lambda_g_optimal))
 
 # predict ratings on test set
 predicted_ratings <- test_set %>% 
@@ -216,27 +224,27 @@ predicted_ratings[is.na(predicted_ratings)] <- mu
 rmse_test <- RMSE(predicted_ratings, test_set$rating)
 
 
-
 # #####################################################
 # compute RMSE for predictions on validation set
 # #####################################################
 
-predicted_ratings <- validation %>% mutate(quarter = round_date(as_datetime(timestamp), "quarter")) %>%
+predicted_ratings <- validation %>% 
+  mutate(quarter = round_date(as_datetime(timestamp), "quarter")) %>%
   left_join(movie_avgs, by='movieId') %>%
   left_join(user_avgs, by='userId') %>%
   left_join(genre_avgs, by = 'genres') %>%
   left_join(quarter_avgs, by = 'quarter') %>%
-  mutate(pred = mu + b_m + b_u + b_g + b_q) %>%
-  pull(pred)
+  mutate(pred = mu + b_m + b_u + b_g + b_q) %>% pull(pred)
+
 predicted_ratings[is.na(predicted_ratings)] <- mu
 
 rmse_validation <- RMSE(predicted_ratings, validation$rating)
 
 
-
 # #####################################################
 # save objects file for upload in Rmd
 # #####################################################
+
 save(edx, lambdas, lambda_m_optimal, lambda_u_optimal, lambda_g_optimal, rmse_test,
      rmse_validation, test_set, train_set, validation, file = "rmd_objects.RData")
 
@@ -246,21 +254,26 @@ save(edx, lambdas, lambda_m_optimal, lambda_u_optimal, lambda_g_optimal, rmse_te
 # ave ratings by genres (movies with only 1 genre)
 # edx %>% filter(genres %in% c("Action", "Adventure", "Animation", "Children", 
 #   "Comedy", "Drama", "Fantasy", "IMAX", "Musical", "Sci-Fi", "Thriller", "War")) %>%  
-#   group_by(genres) %>% summarize(avg_rating = mean(rating)) %>% mutate(genres = reorder(genres, avg_rating)) %>% 
-#   ggplot(aes(genres, avg_rating)) + geom_col()+ theme(axis.text.x = element_text(angle = 90, hjust = 1)) + 
+#   group_by(genres) %>% summarize(avg_rating = mean(rating)) %>% 
+# mutate(genres = reorder(genres, avg_rating)) %>% 
+#   ggplot(aes(genres, avg_rating)) + geom_col() + 
+# theme(axis.text.x = element_text(angle = 90, hjust = 1)) + 
 #   labs(x = "Genre", y = "Average")
 # 
 # # average ratings from users giving >500 ratings
-# edx %>% group_by(userId) %>% filter(n()>500) %>% summarize(user = "Users with more than 500 ratings", avg_rating = mean(rating)) %>% 
-#   ggplot(aes(user, avg_rating)) + geom_boxplot() + geom_jitter(width = 0.03, alpha = .1) +
-#   xlab("") + ylab("Average rating")
+# edx %>% group_by(userId) %>% filter(n()>500) %>% 
+# summarize(user = "Users with more than 500 ratings", avg_rating = mean(rating)) %>% 
+#   ggplot(aes(user, avg_rating)) + geom_boxplot() + 
+# geom_jitter(width = 0.03, alpha = .1) + xlab("") + ylab("Average rating")
 # 
 # # low ratings histogram
-# edx %>% group_by(genres) %>% mutate(count = n()) %>% ungroup() %>% filter(count < 25) %>% 
-#   group_by(count) %>% summarize(nratings = n()) %>% ggplot(aes(count, nratings, fill = count)) + 
-#   geom_bar(stat = "identity") + labs(x = "Number of Ratings", y = "Number of Genres") + 
-#   scale_fill_gradient2(low = "red3", high = "forestgreen", mid = "grey79", midpoint = 16,
-#  breaks=c(3,23),labels=c("Worse","Better"))+ theme(legend.title = element_blank()) 
+# edx %>% group_by(genres) %>% mutate(count = n()) %>% ungroup() %>% 
+# filter(count < 25) %>% group_by(count) %>% summarize(nratings = n()) %>% 
+# ggplot(aes(count, nratings, fill = count)) + geom_bar(stat = "identity") + 
+# labs(x = "Number of Ratings", y = "Number of Genres") + 
+#   scale_fill_gradient2(low = "red3", high = "forestgreen", mid = "grey79", 
+# midpoint = 16, breaks=c(3,23),labels=c("Worse","Better")) + 
+# theme(legend.title = element_blank()) 
 #   
 # train_set %>% mutate(week = round_date(as_datetime(timestamp), unit = "week")) %>%
 #   group_by(week) %>%
